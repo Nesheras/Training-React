257077f25834a9815841653faceff50a9575a59d
Nesheras <karoshop@mail.ru>
2024-04-27 17:24:47 +0300

    Настроил роутер

---
+++ src/Pages/Menu.jsx
@@ -0,0 +1,5 @@
+export function Menu(){
+    return(
+        <p>Меню</p>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './Router/Router.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider stort = {store}>
-    <App />
+    <Provider store = {store}>
+    <RouterProvider router={router}/>
+
     </Provider>
+
   </React.StrictMode>,
 )
